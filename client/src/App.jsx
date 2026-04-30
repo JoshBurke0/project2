c4e248a38b7b41d9eac5cccf1220666cd8fc78cf
JoshBurke0 <josburk29@nuevaschool.org>
2026-04-30 11:31:46 -0700

    fleshed out the static frame

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,13 +5,33 @@ export default function App() {
   const [TaskName, setTaskName] = useState('Testing Task')
   return(
     <>
-      <div id="frame">
-        <div id="titleDiv">
-          <h1 id="title">To Do List</h1>
+      <div class="wrapper">
+        <div id="listFrame">
+          <div id="listTitleFrame">
+            <h1 id="listTitle">To Do List</h1>
+          </div>
+          <div id="innerFrame">
+            {/* <p>Testing App</p>
+            <Task TaskName = {TaskName}/> */}
+          </div>
         </div>
-        <div id="innerFrame">
-          <p>Testing App</p>
-          <Task TaskName = {TaskName}/>
+        <div id="taskFrame">
+          <div id="taskTitleFrame">
+            <h1 id="taskTitle">Create Task</h1>
+          </div>
+          <div id="setInfo">
+              <div id="setWriting">
+                <div id="setTitle">
+                  <h2>Title</h2>
+                  <input id="titleInput"></input>
+                </div>
+                <div id="setDesc"></div>
+              </div>
+              <div id="setDropdown">
+                <div id="setDate"></div>
+                <div id="setPriority"></div>
+              </div>
+          </div>
         </div>
       </div>
     </>
